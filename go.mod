c9d0bd8390f242fdabb343f0d03cfeb705555ff0
xcsk <lorgnette@zoho.com.cn>
2021-02-21 22:29:06 +0800

    Fix title

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/xcsk/apprun
 
 go 1.15
 
-require github.com/xcsk/breakline v0.0.0-20210221140107-35808066553a
+require github.com/xcsk/breakline v0.0.0-20210221142605-ef517b85c6df
--- main.go
+++ main.go
@@ -5,9 +5,14 @@ import (
 	"time"
 
 	bl "github.com/xcsk/breakline"
+	tl "github.com/xcsk/breakline/title"
 )
 
 func main() {
 	fmt.Printf("⏰: %s\n", time.Now().Format("2006-01-02T15:04:05-0700"))
 	fmt.Println(bl.BreakLine(28))
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(tl.Nao())
+	}
 }
